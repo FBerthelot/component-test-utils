620eb8ca670c61e1a3b26de33098dd26c8d680bb
FBerthelot <florent.berthelot35@gmail.com>
2019-07-26 17:41:21 +0200

    :white_check_mark: add test of default export

--- packages/component-test-utils-react/index.spec.js
+++ packages/component-test-utils-react/index.spec.js
@@ -1,5 +1,11 @@
-describe('component-test-utils-react', () => {
-  it('should export by default an object with all methods', () => {});
+import lib, {shallow} from './index';
 
-  it('should export shallow method directly', () => {});
+describe('component-test-utils-react', () => {
+  it('should export by default an object with all methods', () => {
+    expect(typeof lib.shallow).toBe('function');
+  });
+
+  it('should export shallow method directly', () => {
+    expect(typeof shallow).toBe('function');
+  });
 });
