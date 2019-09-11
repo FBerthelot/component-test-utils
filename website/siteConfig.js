855ba8106a980dd56c2179189e47f34bef09783f
FBerthelot <florent.berthelot35@gmail.com>
2019-09-11 18:18:03 +0200

    :construction_worker: add netlify for check documentation

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,7 +24,7 @@ const siteConfig = {
   title: 'Component test utils',
   tagline: 'Test component in standardized way',
   url: 'https://component-test-utils.berthelot.io',
-  cname: 'component-test-utils.berthelot.io', // Your website URL
+  cname: process.env.DOC_CNAME, // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
